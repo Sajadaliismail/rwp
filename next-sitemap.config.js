fff43a5b3161199df46bf6d90c5bf3ebffc539e1
Sajad Ali Ismail <151859102+Sajadaliismail@users.noreply.github.com>
2025-06-11 08:57:33 +0000

    sitemaps

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,11 +2,34 @@
 module.exports = {
   siteUrl: 'https://royalwoodpackers.in',
   generateRobotsTxt: true,
-  sitemapSize: 5000,           // split if more than 5k URLs
+  sitemapSize: 5000,
   robotsTxtOptions: {
-    policies: [
-      { userAgent: '*', allow: '/' },
-    ],
-    // additionalSitemaps: ['https://royalwoodpackers.in/server-sitemap-index.xml'],
+    policies: [{ userAgent: '*', allow: '/' }],
+  },
+  additionalPaths: async (config) => {
+    const dynamicUrls = [
+      'https://www.royalwoodpackers.in/',
+      'https://www.royalwoodpackers.in/products',
+      'https://www.royalwoodpackers.in/contact',
+      'https://www.royalwoodpackers.in/request-rate',
+      'https://www.royalwoodpackers.in/products/rubber-wood-pallets',
+      'https://www.royalwoodpackers.in/products/wooden-boxes',
+      'https://www.royalwoodpackers.in/products/plywood-pallets',
+      'https://www.royalwoodpackers.in/get-rates',
+      'https://www.royalwoodpackers.in/products/country-wood-pallets',
+      'https://www.royalwoodpackers.in/products/painted-wood-pallets',
+      'https://www.royalwoodpackers.in/products/pine-wood-pallets',
+      'https://www.royalwoodpackers.in/products/plastic-pallets',
+      'https://www.royalwoodpackers.in/products/wooden-crates',
+      'https://www.royalwoodpackers.in/products/wooden-planks',
+      'https://www.royalwoodpackers.in/products/plywood',
+      'https://www.royalwoodpackers.in/products/veneer-sheets',
+      'https://www.royalwoodpackers.in/products/rented-pallets',
+    ];
+
+    return dynamicUrls.map(url => ({
+      loc: url,
+      lastmod: new Date().toISOString(), // you can customize lastmod if needed
+    }));
   },
 };
